f99a8adaf729e1e8bb7b692feec3dca67f16cd0e
Bilal <786.parrot@gmail.com>
2024-12-17 10:00:27 +0500

    .

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,25 +10,25 @@ const HeroSection = () => {
                         <div className="col-lg-6 col-md-12">
                             <div className="personal-portfolio-banner-content">
                                 <span className="sub-title"
-                                data-aos="fade-in" data-aos-duration={1200} data-aos-delay={100}
+                                    data-aos="fade-in" data-aos-duration={1200} data-aos-delay={100}
                                 >Hello I'm</span>
                                 <h1
-                                data-aos="fade-in"  data-aos-duration={1200} data-aos-delay={200}
+                                    data-aos="fade-in" data-aos-duration={1200} data-aos-delay={200}
                                 >Bilal shafeeQ</h1>
                                 <h4
-                                data-aos="fade-in" data-aos-duration={1200} data-aos-delay={300}
+                                    data-aos="fade-in" data-aos-duration={1200} data-aos-delay={300}
                                 >MERN Stack Developer</h4>
                                 <p
-                                data-aos="fade-in" data-aos-duration={1200} data-aos-delay={400}
+                                    data-aos="fade-in" data-aos-duration={1200} data-aos-delay={400}
                                 >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
-                                <Link to="/" className="default-btn-two"
-                                    data-aos="fade-in" data-aos-duration={1200} data-aos-delay={400} 
-                                    >Connect With Me</Link>
+                                <Link to="/" className="default-btn-two btnClickEffect" style={{}}>
+                                    Connect With Me
+                                </Link>
                             </div>
                         </div>
                         <div className="col-lg-6 col-md-12">
                             <div className="personal-portfolio-banner-image"
-                            data-aos="fade-up" data-aos-duration={1200} data-aos-delay={600}
+                                data-aos="fade-up" data-aos-duration={1200} data-aos-delay={600}
                             >
                                 <img src="/images/banner-profile.webp" alt="image" />
                             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,164 +29,166 @@ const Navbar = () => {
             <Link to="/" className="navbar-brand">
               <img src="/images/logo.webp" alt="logo" />
             </Link>
-            <button onClick={() => setHamburgerList(!hamburgerList)} 
-            className="navbar-toggler collapsed" type="button">
+            <button onClick={() => setHamburgerList(!hamburgerList)}
+              className="navbar-toggler collapsed" type="button">
               <span className="icon-bar top-bar" />
               <span className="icon-bar middle-bar" />
               <span className="icon-bar bottom-bar" />
-            </button>     
-            <div className={hamburgerList ? null : "collapse navbar-collapse" } id='okkk'>
-                <ul className="navbar-nav">
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/">Home <FaAngleDown /></Link>
-                    <ul className="dropdown-menu mega-dropdown-menu">
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Default Home</Link>
-                        <Link className="nav-link" to="/">Creative Agency</Link>
-                        <Link className="nav-link" to="/">IT Agency</Link>
-                        <Link className="nav-link" to="/">Marketing Agency</Link>
-                        <Link className="nav-link" to="/">Portfolio Agency</Link>
-                        <Link className="nav-link" to="/">Studio Agency</Link>
-                        <Link className="nav-link" to="/">Business Agency</Link>
-                        <Link className="nav-link" to="/">Startup Agency</Link>
-                        <Link className="nav-link" to="/">Software Startup 
+            </button>
+            <div className={hamburgerList ? null : "collapse navbar-collapse"} id='okkk'>
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Home <FaAngleDown /></Link>
+                  <ul className="dropdown-menu mega-dropdown-menu">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Default Home</Link>
+                      <Link className="nav-link" to="/">Creative Agency</Link>
+                      <Link className="nav-link" to="/">IT Agency</Link>
+                      <Link className="nav-link" to="/">Marketing Agency</Link>
+                      <Link className="nav-link" to="/">Portfolio Agency</Link>
+                      <Link className="nav-link" to="/">Studio Agency</Link>
+                      <Link className="nav-link" to="/">Business Agency</Link>
+                      <Link className="nav-link" to="/">Startup Agency</Link>
+                      <Link className="nav-link" to="/">Software Startup
                         <span className="new">New</span>
-                        </Link>
-                        <Link to="/" className="nav-link" >Digital Marketing 
+                      </Link>
+                      <Link to="/" className="nav-link" >Digital Marketing
                         <span className="new">New</span>
-                        </Link>
-                        <Link to="/" className="nav-link" >Business Consulting 
+                      </Link>
+                      <Link to="/" className="nav-link" >Business Consulting
                         <span className="new">New</span>
-                        </Link>
-                        <Link to="/" className="nav-link" >Freelancer Portfolio 
+                      </Link>
+                      <Link to="/" className="nav-link" >Freelancer Portfolio
                         <span className="new">New</span>
-                        </Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Default Home Two</Link>
-                        <Link className="nav-link" to="/">Creative Agency Two</Link>
-                        <Link className="nav-link" to="/">IT Agency Two</Link>
-                        <Link className="nav-link" to="/">Marketing Agency Two</Link>
-                        <Link className="nav-link" to="/">Portfolio Agency Two</Link>
-                        <Link className="nav-link" to="/">Studio Agency Two</Link>
-                        <Link className="nav-link" to="/">Business Agency Two</Link>
-                        <Link className="nav-link" to="/">Startup Agency Two</Link>
-                        <Link className="nav-link" to="/">App Showcase 
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Default Home Two</Link>
+                      <Link className="nav-link" to="/">Creative Agency Two</Link>
+                      <Link className="nav-link" to="/">IT Agency Two</Link>
+                      <Link className="nav-link" to="/">Marketing Agency Two</Link>
+                      <Link className="nav-link" to="/">Portfolio Agency Two</Link>
+                      <Link className="nav-link" to="/">Studio Agency Two</Link>
+                      <Link className="nav-link" to="/">Business Agency Two</Link>
+                      <Link className="nav-link" to="/">Startup Agency Two</Link>
+                      <Link className="nav-link" to="/">App Showcase
                         <span className="new">New</span>
-                        </Link>
-                        <Link className="nav-link" to="/">Personal Portfolio 
+                      </Link>
+                      <Link className="nav-link" to="/">Personal Portfolio
                         <span className="new">New</span>
-                        </Link>
-                        <Link className="nav-link" to="/">SaaS Startup 
+                      </Link>
+                      <Link className="nav-link" to="/">SaaS Startup
                         <span className="new">New</span>
-                        </Link>
-                        <Link className="nav-link" to="/">Cyber Security Agency 
+                      </Link>
+                      <Link className="nav-link" to="/">Cyber Security Agency
                         <span className="new">New</span>
-                        </Link>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/">Services <FaAngleDown />
-                    </Link>
-                    <ul className="dropdown-menu">
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Services Style 1</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Services Style 2</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Services Style 3</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Services Style 4</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Service Details</Link>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/">About us</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/">Portfolio <FaAngleDown /></Link>
-                    <ul className="dropdown-menu">
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Portfolio</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Portfolio Details</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Portfolio Details 2</Link>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/">Pages <FaAngleDown /></Link>
-                    <ul className="dropdown-menu">
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">About</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Services</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Services Details</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Portfolio</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Portfolio Details</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Team</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Pricing</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">FAQ</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">404 error</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Coming Soon</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Contact</Link>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="nav-item">
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Services <FaAngleDown />
+                  </Link>
+                  <ul className="dropdown-menu">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Services Style 1</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Services Style 2</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Services Style 3</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Services Style 4</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Service Details</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">About us</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Portfolio <FaAngleDown /></Link>
+                  <ul className="dropdown-menu">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Portfolio</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Portfolio Details</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Portfolio Details 2</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Pages <FaAngleDown /></Link>
+                  <ul className="dropdown-menu">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">About</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Services</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Services Details</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Portfolio</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Portfolio Details</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Team</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Pricing</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">FAQ</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">404 error</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Coming Soon</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Contact</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
                   <Link className="nav-link" to="/">Blog <FaAngleDown /></Link>
-                    <ul className="dropdown-menu">
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Blog Grid</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Blog Right Sidebar</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Blog Left Sidebar</Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link className="nav-link" to="/">Blog Details</Link>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/">Contact Us</Link>
-                  </li>
-                </ul>
-                <div className="others-options">
-                  <Link className="btn btn-primary" to="/">Get Started</Link>
-                </div>
+                  <ul className="dropdown-menu">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Blog Grid</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Blog Right Sidebar</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Blog Left Sidebar</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/">Blog Details</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Contact Us</Link>
+                </li>
+              </ul>
+              <div className="others-options">
+                <Link className="btn btn-primary btnClickEffect" to="/">
+                  Get Started
+                </Link>
               </div>
+            </div>
           </div>
         </nav>
       </div>
