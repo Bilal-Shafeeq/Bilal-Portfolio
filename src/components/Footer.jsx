30f2c26148d019e45d91d71ee1cb647afa70810f
Bilal <786.parrot@gmail.com>
2024-12-17 10:22:18 +0500

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ import { IoLocationOutline } from "react-icons/io5";
 import { FaHeadset } from "react-icons/fa";
 import { BiEnvelope } from "react-icons/bi";
 import GoToTop from './GoToTop';
+import { Link } from 'react-router-dom';
 
 
 
@@ -16,12 +17,12 @@ const Footer = () => {
           <div className="row ">
             <div className="col-lg-3 col-sm-6">
               <div className="single-footer-widget"
-               data-aos="fade-in" data-aos-duration={1200} data-aos-delay={100}
+                data-aos="fade-in" data-aos-duration={1200} data-aos-delay={100}
               >
                 <div className="logo">
-                  <a href="/">
+                  <Link to="/">
                     <img src="/images/logo.webp" alt="Logo" />
-                  </a>
+                  </Link>
                 </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
                 <ul className="social-links">
@@ -50,44 +51,52 @@ const Footer = () => {
             </div>
             <div className="col-lg-3 col-md-6 col-sm-6">
               <div className="single-footer-widget"
-               data-aos="fade-in" data-aos-duration={1200} data-aos-delay={200} 
+                data-aos="fade-in" data-aos-duration={1200} data-aos-delay={200}
               >
                 <h3>Explore</h3>
                 <ul className="list">
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">About</a></li>
-                  <li><a href="#">Services</a></li>
-                  <li><a href="#">Portfolio</a></li>
-                  <li><a href="#">Team</a></li>
+                  <li><Link to="/">Home</Link></li>
+                  <li><Link to="/">About</Link></li>
+                  <li><Link to="/">Services</Link></li>
+                  <li><Link to="/">Portfolio</Link></li>
+                  <li><Link to="/">Team</Link></li>
                 </ul>
               </div>
             </div>
             <div className="col-lg-3 col-sm-6">
               <div className="single-footer-widget"
-              data-aos="fade-in" data-aos-duration={1200} data-aos-delay={300}
+                data-aos="fade-in" data-aos-duration={1200} data-aos-delay={300}
               >
                 <h3>Quick Links</h3>
                 <ul className="list">
-                  <li><a href="#">Contact Us</a></li>
-                  <li><a href="#">Pricing</a></li>
-                  <li><a href="#">Faq</a></li>
-                  <li><a href="#">Privacy Policy</a></li>
-                  <li><a href="#">Terms &amp; Conditions</a></li>
+                  <li><Link to="/">Contact Us</Link></li>
+                  <li><Link to="/">Pricing</Link></li>
+                  <li><Link to="/">Faq</Link></li>
+                  <li><Link to="/">Privacy Policy</Link></li>
+                  <li><Link to="/">Terms &amp; Conditions</Link></li>
                 </ul>
               </div>
             </div>
             <div className="col-lg-3 col-sm-6">
               <div className="single-footer-widget"
-              data-aos="fade-in" data-aos-duration={1200} data-aos-delay={400}
+                data-aos="fade-in" data-aos-duration={1200} data-aos-delay={400}
               >
                 <h3>Get in Touch</h3>
                 <ul className="get-in-touch">
                   <li>
                     <IoLocationOutline className="hhh" style={{ marginTop: "4px" }} /> 2750 Quadra Street Punjab, Pakistan.
                   </li>
-                  <li><FaHeadset className="hhh" style={{ marginTop: "8px" }} /><a href="tel:#">+098-7654-321</a><br /><a href="tel:#">+123-4567-890</a></li>
-                  <li><BiEnvelope className="hhh" style={{ marginTop: "3px" }} /><a href="#"><span>bilal@pungent.com</span></a>
-                    <a href="#"><span>support@pungent.com</span></a>
+                  <li>
+                    <FaHeadset className="hhh" style={{ marginTop: "8px" }} />
+                    <Link to="/">+098-7654-321</Link>
+                    <br />
+                    <Link to="/">+123-4567-890</Link>
+                  </li>
+                  <li><BiEnvelope className="hhh" style={{ marginTop: "3px" }} />
+                    <Link to="/"><span>bilal@pungent.com</span>
+                    </Link>
+                    <Link to="/"><span>support@pungent.com</span>
+                    </Link>
                   </li>
                 </ul>
               </div>
@@ -97,7 +106,7 @@ const Footer = () => {
         <div className="copyright-area">
           <div className="container">
             <p>Copyright © 2023 Pungent. All Rights Reserved By
-              <a href="#" target="_blank" style={{ marginLeft: "4px" }}>Bilal</a>
+              <Link to="/" style={{ marginLeft: "4px" }}>Bilal</Link>
             </p>
             <GoToTop />
           </div>
